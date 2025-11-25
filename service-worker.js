bcce48e40e0e3bcddd2f4283d02925dbb88a21df
joshneedham89-debug <joshneedham89@gmail.com>
2025-11-25 06:31:18 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,31 @@
-self.addEventListener('install',e=>{self.skipWaiting();});
+
+const CACHE_NAME = 'pm-final-cache-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => (k === CACHE_NAME ? null : caches.delete(k)))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
