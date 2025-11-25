125d1cbfbac28a47ed013da5243c4243d2cb4471
joshneedham89-debug <joshneedham89@gmail.com>
2025-11-24 22:19:25 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,36 @@
-self.addEventListener('install',e=>{self.skipWaiting();});
+const CACHE_NAME = 'pm-shell-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json',
+  'icon_16.png',
+  'icon_32.png',
+  'icon_128.png',
+  'icon_180.png',
+  'icon_192.png',
+  'icon_512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => (k === CACHE_NAME ? null : caches.delete(k)))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});;
